dd0c8c623f017f97e8ff7c16384800449159bf3a
Viktoria Shchypalkina <37249354+helavika@users.noreply.github.com>
2019-01-22 20:58:42 +0200

    Add files via upload

--- test1/js/index.js
+++ test1/js/index.js
@@ -6,10 +6,10 @@ var row2 = 'zxcvbnm,./';
 
 var allRows = (row + row1 + row2);
 
-console.log(allRows);
+console.log(`${allRows}`);
 
-console.log(row.length, row1.length, row2.length);
+console.log(`${row.length}, ${row1.length}, ${row2.length}`);
 
-console.log(row.charAt(0), row.charAt(11), row1.charAt(0), row1.charAt(10), row2.charAt(0), row2.charAt(9));
+console.log(`${row.charAt(0)}, ${row.charAt(row.length -1)}, ${row1.charAt(0)}, ${row1.charAt(row1.length -1)}, ${row2.charAt(0)}, ${row2.charAt(row2.length -1)}`);
 
-console.log(allRows.indexOf('['), allRows.indexOf(']'));
+console.log(`${allRows.indexOf('[')}, ${allRows.indexOf(']')}`);
